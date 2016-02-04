04bc8b196280edc1d21184cbfcaba9ea0688e060
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-02-04 12:57:13 +0000

    add hypothesis test for left_subtree_size

--- tests/test_merkle.py
+++ tests/test_merkle.py
@@ -1,4 +1,6 @@
 import pytest
+from hypothesis import given
+from hypothesis.strategies import integers
 from integritytool.runner import *
 
 def test_leaf_hash():
@@ -18,3 +20,16 @@ def test_left_subtree_size():
 	assert left_subtree_size(127) == 64
 	assert left_subtree_size(128) == 64
 	assert left_subtree_size(129) == 128
+
+@given(integers(min_value=2))
+def test_left_subtree_size_gen(n):
+        k = left_subtree_size(n)
+
+        # assert k is a power of 2
+        # from http://www.graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
+        assert (k & (k - 1)) == 0
+
+        # assert k the largest power of 2 smaller than n
+        # from RFC 6962 § 2.1
+        assert k < n
+        assert n <= 2*k
